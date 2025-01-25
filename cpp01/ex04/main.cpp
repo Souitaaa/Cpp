485ff7684ce2c20f7fe6fab64553ced97a98d070
csouita <csouita@c3r5p7.1337.ma>
2025-01-25 18:15:25 +0100

    finished ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:45:25 by csouita           #+#    #+#             */
-/*   Updated: 2025/01/24 22:47:37 by csouita          ###   ########.fr       */
+/*   Updated: 2025/01/25 18:13:26 by csouita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,48 @@
 #include <string>
 
 
-int main(int ac , char *av[]) 
+int main(int ac, char *av[]) 
 {
-    std::string search = av[1] ;
-    std::string replace = av[2] ;
-    const char* filename = "main.cpp";
-    const char* outname = "main.cpp.replace";
-    std::ofstream outfile(outname);
-    std::ifstream file(filename);
-    if(ac != 4)
+    if (ac != 4)
     {
-        std::cout << "Error: Wrong number of arguments" << std::endl;
-        std::cout << "Please enter the filename and the strings S1 and S2" << std::endl;
+        std::cerr << "Error: Wrong number of arguments" << std::endl;
+        std::cerr << "Please enter the filename and the strings S1 and S2" << std::endl;
         return 1;
     }
-    if (!outfile.is_open())
-    {
-        std::cerr << "Failed to open the outfile: " << outname << std::endl;
-    }
-    if (file.is_open()) 
-    {
-        std::string line;
 
-        while (std::getline(file, line)) {
-            outfile << line << std::endl;
-        }
-        file.close();
-    } 
-    outfile.close();
-    std::ifstream infile(outname);
-    if(infile)
-    {        
-        std::string line2;
-        while(std::getline(infile, line2))
-            std::cout << line2 << std::endl;
-        infile.close();
-    }
+    std::string search = av[2];
+    std::string replace = av[3];
+    std::string  filename = av[1];
+    std::string  outname ;
+    std::size_t dotPosition = (filename).find_last_of('.');
+    if (dotPosition != std::string::npos)
+        outname = filename.substr(0 , dotPosition) + ".replace";
     else 
-        std::cerr << "Failed to open the file: " << outname << std::endl;
+        outname = filename + ".replace";
+
+    
+    std::ofstream outfile(outname.c_str());
+    std::ifstream file(filename.c_str());
+    if (!outfile.is_open() || !file.is_open()) 
+    {
+        std::cerr << "Failed to open the file: " << filename << std::endl;
+        return 1;
+    }
+
+    std::string line;    
+    while(std::getline(file , line))
+    {
+        std::size_t pos = 0 ;
+        while((pos = line.find(search , pos)) != std::string::npos)
+        {
+            outfile << line.substr(0, pos) << replace;
+            pos += search.length();
+            line = line.substr(pos);
+            pos = 0;
+        }
+        outfile << line << std::endl;
+    }
+    file.close();
+    outfile.close();
     return 0;
 }
