5ddc68fd4f5ab88a21430b7413c8e1e8408b4a65
csouita <csouita@c3r10p3.1337.ma>
2025-02-06 18:37:08 +0100

    modified the main function

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 00:31:07 by csouita           #+#    #+#             */
-/*   Updated: 2025/02/06 18:24:19 by csouita          ###   ########.fr       */
+/*   Updated: 2025/02/06 18:35:57 by csouita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,29 @@
 #include "WrongAnimal.hpp"
 #include "Brain.hpp"
 
-
 int main()
-{
+{   
     Animal *animals[10];
-    for (int i = 0 ; i < 10; i++)
+    for (int i = 0 ; i < 10 ; i++)
     {
         if (i % 2 == 0)
             animals[i] = new Dog();
         else
             animals[i] = new Cat();
     }
-    for (int i = 0 ; i < 10; i++)
+    for (int i = 0 ; i < 10 ; i++)
     {
         animals[i]->makeSound();
     }
-    for (int i = 0 ; i < 10; i++)
+    for (int i = 0 ; i < 10 ; i++)
     {
         delete animals[i];
     }
-    std::cout << "---------------------" << std::endl;
     
+    std::cout << "-------------------" << std::endl;    
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    delete j;//should not create a leak
+    delete i;
+    return 0;
 }
