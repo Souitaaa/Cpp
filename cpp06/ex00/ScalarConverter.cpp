65c34efa78b45da23e8b0cb948cfdfbb8dd4ac14
csouita <souita404@gmail.com>
2025-07-09 16:53:09 +0100

    added cpp06

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 00:52:45 by mongool           #+#    #+#             */
+/*   Updated: 2025/07/09 16:52:15 by csouita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::~ScalarConverter() {}
+
+void ScalarConverter::convert(const std::string input) {
+        
+}  
---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 00:52:45 by mongool           #+#    #+#             */
+/*   Updated: 2025/07/09 16:22:52 by csouita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include <cmath>
+
+
+class ScalarConverter {
+    public :
+        ScalarConverter();
+        ~ScalarConverter();
+        void convert(const std::string input);
+}
