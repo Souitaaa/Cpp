2743ca72fda1cc482d073eeaad6615d36c9db8b5
csouita <souita404@gmail.com>
2025-07-28 17:32:08 +0100

    finished cpp08/ex01

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,11 +6,12 @@
 /*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 00:52:45 by mongool           #+#    #+#             */
-/*   Updated: 2025/07/28 17:09:01 by csouita          ###   ########.fr       */
+/*   Updated: 2025/07/28 17:30:01 by csouita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include <limits>
 
 Span::Span(unsigned int n) : N(n) {}
 
@@ -26,12 +27,28 @@ void Span::addNumber(int number)
 
 unsigned int Span::shortestSpan() const
 {
-    unsigned int minSpan ;
+    unsigned int minSpan = std::numeric_limits<unsigned int>::max();
     if(numbers.size() < 2)
         throw std::runtime_error("requires at least 2 numbers");
-    std::vector<int> sortedVec;
+    std::vector<int> sortedVec(numbers);
     std::sort(sortedVec.begin(),sortedVec.end());
-    for(size_t i = 0 ; sortedVec.size() ; i++)
+    for(size_t i = 1 ; sortedVec.size() > i ; i++)
+    {
+        unsigned int temp = sortedVec[i] - sortedVec[i - 1];
+        if(minSpan > temp)
+            minSpan = temp; 
+    }
+    return minSpan;
+}
+
+unsigned int Span::longestSpan() const
+{
+    unsigned int minSpan = std::numeric_limits<unsigned int>::min();
+    if(numbers.size() < 2)
+        throw std::runtime_error("requires at least 2 numbers");
+    std::vector<int> sortedVec(numbers);
+    std::sort(sortedVec.begin(),sortedVec.end());
+    for(size_t i = 1 ; sortedVec.size() > i ; i++)
     {
         unsigned int temp = sortedVec[i] - sortedVec[i - 1];
         if(minSpan < temp)
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: csouita <csouita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 00:52:45 by mongool           #+#    #+#             */
-/*   Updated: 2025/07/28 17:07:31 by csouita          ###   ########.fr       */
+/*   Updated: 2025/07/28 17:31:37 by csouita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int main() {
     try {
         Span sp(5);
         sp.addNumber(6);
-        sp.addNumber(3);
+        sp.addNumber(34);
         sp.addNumber(17);
-        sp.addNumber(9);
+        sp.addNumber(23);
         sp.addNumber(11);
 
-        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Shortest span: " << sp.longestSpan() << std::endl;
     } catch (const std::exception& e) {
         std::cout << "Error: " << e.what() << std::endl;
     }
